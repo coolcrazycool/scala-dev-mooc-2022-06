2c6082504d6378e14d9e257fb0bd4117d1443689
18672585 <darazumovskiy@sberbank.ru>
2022-10-07 09:54:35 +0300

    Homework HKT

--- src/main/scala/module2/homework_hkt_impllicts.scala
+++ src/main/scala/module2/homework_hkt_impllicts.scala
@@ -1,6 +1,6 @@
 package module2
 
-object homework_hkt_impllicts{
+object homework_hkt_impllicts extends App{
 
     /**
       * 
@@ -10,13 +10,28 @@ object homework_hkt_impllicts{
       *   val r2 = println(tupleF(list1, list2))
       * 
       */
-    def tupleF[F[_], A, B](fa: F[A], fb: F[B]) = ???
+    def tupleF[F[_], A, B](fa: F[A], fb: F[B])(implicit ba: F[A] => Bindable[F, A], bb: F[B] => Bindable[F, B]): F[(A, B)] = {
+      fa.flatMap(a => fb.map(b => (a, b)))
+    }
 
 
-    trait Bindable[F[_], A] {
+  trait Bindable[F[_], A] {
         def map[B](f: A => B): F[B]
         def flatMap[B](f: A => F[B]): F[B]
     }
+  object Bindable {
+    implicit def BindableList[A](lst: List[A]): Bindable[List, A] = new Bindable[List, A] {
+      override def map[B](f: A => B): List[B] = lst.map(f)
+
+      override def flatMap[B](f: A => List[B]): List[B] = lst.flatMap(f)
+    }
+
+    implicit def BindableOption[A](opt: Option[A]): Bindable[Option, A] = new Bindable[Option, A] {
+      override def map[B](f: A => B): Option[B] = opt.map(f)
+
+      override def flatMap[B](f: A => Option[B]): Option[B] = opt.flatMap(f)
+    }
+  }
 
 
   val optA: Option[Int] = Some(1)
